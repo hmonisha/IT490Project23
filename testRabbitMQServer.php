be8229f15e5caa237ad5e245550befe26af1e315
Jacob Jones <jnj22@njit.edu>
2023-02-02 10:42:26 -0500

    Added authentication with the login parameter on RabbitMQServer with password hashing Added appropriate password hash to db for testing - Will give it out later today when meeting. Set up returns for authentication for approval and denial of access Need to set up the login to be able to do more on the server. Need to set up the client to allow for easier login changes to easily test new accounts.
    There is no level of authorization at this juncture just purely authentication.
    Should probably added salting and peppering to that password hashes to increase security at some stage.
    Should added PPK encryption to the network traffic to protect hashes in the air at some stage.

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -4,8 +4,35 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+///Extract this information out
+$dbUser = "admin";
+$dbPass = "IT490password";
+$serverName = "localhost";
+$loginDBName = "IT490";
+
 function doLogin($username,$password)
 {
+	try{
+		global $dbUser, $dbPass, $serverName, $loginDBName;
+		$dbConn = new PDO("mysql:host=$serverName;dbname=$loginDBName", $dbUser, $dbPass);
+		$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$loginStmt = $dbConn->prepare("SELECT passhash FROM userLogin WHERE username = :user");
+		$loginStmt->execute([':user' => $username]);
+		if($loginStmt->rowCount() < 0 or $loginStmt->rowCount() > 1){
+			echo "Error: Found incorrect information for user $username";
+			return False;
+		} else {
+			$passHash = $loginStmt->fetch()[0];
+			if ($password == $passHash) {
+				return True;
+			}
+			return False;
+		}
+		return False;
+	}catch(PDOExcept $v) {
+		echo "Error: " . $e.getMessage();
+		return False;
+	}
     // lookup username in databas
     // check password
     return true;
@@ -20,14 +47,25 @@ function requestProcessor($request)
   {
     return "ERROR: unsupported message type";
   }
-  switch ($request['type'])
+  $login = "";
+  switch (strtolower($request['type']))
   {
-    case "login":
-      return doLogin($request['username'],$request['password']);
+  case "login":
+	  $login = doLogin($request['username'],$request['password']);
+	  if($login){
+	 	return array("returnCode" => '202', 'message'=>"Server received request and approved the login request.");
+
+	  }
+	  else {
+		  return array("returnCode" => '401', 'message'=>"Server received request and denied the login request.");
+
+	  }
+	  break;
     case "validate_session":
       return doValidate($request['sessionId']);
   }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+  $loginStr = $login ? 'True' : 'False';
+  return array("returnCode" => '0', 'message'=>"Server received request and processed and replied with $loginStr");
 }
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
