78b38dd9147e94dc2ac9c56b884d456ed3ab8987
efm24 <90346275+efm24@users.noreply.github.com>
2023-02-07 13:25:05 -0500

    Update login.php
    Changed login.php files with requests

--- sample/login.php
+++ sample/login.php
@@ -1,7 +1,10 @@
 <?php
-
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
 if (!isset($_POST))
+
 {
 	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
 	echo json_encode($msg);
@@ -9,13 +12,40 @@ if (!isset($_POST))
 }
 $request = $_POST;
 $response = "unsupported request type, politely FUCK OFF";
+
 switch ($request["type"])
 {
 	case "login":
 		$response = "login, yeah we can do that";
+        $request = array();
+        $request['type'] = "Login";
+        $request['username'] = $POST['username'];
+        $request['password'] = $POST['password'];
+        $request['message'] = $msg;
+        $response = $client->send_request($request);
+        
 	break;
 }
 echo json_encode($response);
 exit(0);
+
+
+
+
+
+
+
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+
+
 
 ?>
