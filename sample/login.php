b94cb19a5588a758fd5c2d992ccf96c8db78ee3f
Jacob Jones <jnj22@njit.edu>
2023-02-09 13:07:10 -0500

    Revert "Resetting all web stuff to test functionality"
    This reverts commit 7b92eef0833636b66f7eb7e74b88d24a121ab33f.

--- sample/login.php
+++ sample/login.php
@@ -1,7 +1,10 @@
 <?php
-
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
 if (!isset($_POST))
+
 {
 	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
 	echo json_encode($msg);
@@ -9,13 +12,25 @@ if (!isset($_POST))
 }
 $request = $_POST;
 $response = "unsupported request type, politely FUCK OFF";
+
 switch ($request["type"])
 {
 	case "login":
 		$response = "login, yeah we can do that";
+		echo $reponse;
+		$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+        	$request = array();
+        	$request['type'] = "Login";
+        	$request['username'] = $POST['username'];
+        	$request['password'] = $POST['password'];
+        	$request['message'] = "test WILL BE A KEY";
+        	$response = $client->send_request($request);
+        
 	break;
 }
 echo json_encode($response);
 exit(0);
+
+
 
 ?>
