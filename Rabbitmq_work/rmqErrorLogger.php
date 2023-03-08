28ca2e705e87afbd7df4df2a29860f68715923ac
efm24 <90346275+efm24@users.noreply.github.com>
2023-03-08 15:49:40 -0500

    Update rmqErrorLogger.php

--- Rabbitmq_work/rmqErrorLogger.php
+++ Rabbitmq_work/rmqErrorLogger.php
@@ -4,10 +4,9 @@ require_once('../rabbitmqphp_example/get_host_info.inc');
 require_once('../rabbitmqphp_example/rabbitMQLib.inc');
     
     function error_logger($error, $fileName){
+        
             $file = fopen( $fileName . '.txt', "a" );
-            for ($x = 0; $x < count($error); $x++){
-            fwrite( $file, $error[$x] );
-            }
+            for ($x = 0; $x < count($error); $x++){fwrite( $file, $error[$x] );}
             return true;
         }
 
