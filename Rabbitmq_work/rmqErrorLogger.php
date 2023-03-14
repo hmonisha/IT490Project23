02508be8fd6ca6ac3dfbe60a9a201d0f59d1c49a
efm24 <90346275+efm24@users.noreply.github.com>
2023-03-13 23:23:23 -0400

    Update rmqErrorLogger.php

--- Rabbitmq_work/rmqErrorLogger.php
+++ Rabbitmq_work/rmqErrorLogger.php
@@ -4,7 +4,7 @@ require_once('../rabbitmqphp_example/get_host_info.inc');
 require_once('../rabbitmqphp_example/rabbitMQLib.inc');
     
     function error_logger($queue,$error){
-        $fp = "error.log";
+        $fp = "errors.log";
         $error = "[" . date('Y-m-d H:i:s') . "] There was an Error in queue '" . $queue . "': " . $error . "\n";
         $fh = fopen($fp, "a");
         fwrite($fh, $error);
