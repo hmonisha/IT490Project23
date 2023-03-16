7f7b257606d985dee872b4ddd0bbab958971ae4f
Jacob Jones <jacob.jones@veteransoftware.us>
2023-03-16 14:42:12 -0400

    New site built
    Currently functioning WITHOUT RABBIT CONNECTION
    
    SearchPage with basic books
    View page with book info, rating, comments (No buttons for read book, user to rate book, or user to leave comments)
    Registration add to login page

---
+++ NewWebServer/BookLookups.php
@@ -0,0 +1,85 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+if (!isset($_POST))
+
+{
+    $msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
+    echo json_encode($msg);//log
+    exit(0);
+}
+$request = $_POST;
+$response = "unsupported request type, politely FUCK OFF";
+
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer"); //CHANGE TO CORRECT SERVER!!
+
+switch ($request["type"])
+{
+    case "searchBooks":
+        $response = "login, yeah we can do that";
+        echo $response;
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "searchBooks";
+        $rabbitRequest['searchQuery'] = $request['searchQuery'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+
+        break;
+    case "getBook":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "getBook";
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "readBook":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "readBook";
+        $rabbitRequest['username'] = $request['username'];
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "getReviews":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "getReviews";
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "addReview":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "addReview";
+        $rabbitRequest['username'] = $request['addReview'];
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['review'] = $request['review'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "getRating":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "getRating";
+        $rabbitRequest['username'] = $request['getRating'];
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "setRating":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "setRating";
+        $rabbitRequest['username'] = $request['username'];
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['rating'] = $request['rating'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+}
+echo json_encode($response);
+exit(0);
+
+
+
+?>
