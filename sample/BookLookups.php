b1bfef18489da4b74977186b1a0980e29ed639c1
LuisSoriano <luis@LuisSoriano.myguest.virtualbox.org>
2023-03-23 10:03:21 -0400

    pushing a few changes

---
+++ sample/BookLookups.php
@@ -0,0 +1,99 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('event_logger.php');
+
+if (!isset($_POST))
+
+{
+    $msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
+    echo json_encode($msg);//log
+    exit(0);
+}
+$request = $_POST;
+$response = "unsupported request type, politely FUCK OFF";
+
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer"); //CHANGE TO CORRECT SERVER!!
+
+switch (strtolower($request["type"]))
+{
+    case "searchbooks":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "searchBooks";
+        $rabbitRequest['searchQuery'] = $request['searchQuery'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+
+        break;
+    case "getbook":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "getBook";
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "getreadbook":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "getReadBook";
+        $rabbitRequest['username'] = $request['username'];
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "getreadbooks":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "getReadBooks";
+        $rabbitRequest['username'] = $request['username'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "setreadbook":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "setReadBook";
+        $rabbitRequest['username'] = $request['username'];
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "getdiscssionposts":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "getDiscussionPosts";
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "adddiscussionpost":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "addDiscussionPost";
+        $rabbitRequest['username'] = $request['username'];
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['post'] = $request['post'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "getrating":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "getRating";
+        $rabbitRequest['username'] = $request['username'];
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+    case "setrating":
+        $rabbitRequest = array();
+        $rabbitRequest['type'] = "setRating";
+        $rabbitRequest['username'] = $request['username'];
+        $rabbitRequest['bookID'] = $request['bookID'];
+        $rabbitRequest['rating'] = $request['rating'];
+        $rabbitRequest['message'] = "test WILL BE A KEY";
+        $response = $client->send_request($rabbitRequest);
+        break;
+}
+echo json_encode($response);
+exit(0);
+
+
+
+?>
--- sample/login.php
+++ sample/login.php
@@ -2,7 +2,7 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-
+require_once('event_logger.php');
 if (!isset($_POST))
 
 {
@@ -13,12 +13,14 @@ if (!isset($_POST))
 $request = $_POST;
 $response = "unsupported request type, politely FUCK OFF";
 
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer"); //CHANGE TO CORRECT SERVER!!
+
 switch ($request["type"])
 {
 	case "login":
 		$response = "login, yeah we can do that";
-		echo $reponse;
-		$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+		echo $response;
         	$rabbitRequest = array();
         	$rabbitRequest['type'] = "Login";
         	$rabbitRequest['username'] = $request['username'];
@@ -27,6 +29,14 @@ switch ($request["type"])
         	$response = $client->send_request($rabbitRequest);
         
 	break;
+	case "register":
+		$rabbitRequest = array();
+		$rabbitRequest['type'] = "registration";
+		$rabbitRequest['username'] = $request['username'];
+		$rabbitRequest['password'] = $request['password'];
+		$rabbitRequest['message'] = "test WILL BE A KEY";
+		$response = $client->send_request($rabbitRequest);
+		break;
 }
 echo json_encode($response);
 exit(0);
