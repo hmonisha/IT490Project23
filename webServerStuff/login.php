28c98ae7595e27c48fdbebb54e8999aec1641c66
Jacob Jones <jnj22@njit.edu>
2023-03-23 12:08:42 -0400

    Uploading current web server stuff to new branch for editing on better IDE as code needs to be set up to run async

---
+++ webServerStuff/login.php
@@ -0,0 +1,49 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('event_logger.php');
+if (!isset($_POST))
+
+{
+	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
+	echo json_encode($msg);
+	exit(0);
+}
+$request = $_POST;
+$response = "unsupported request type, politely FUCK OFF";
+
+
+
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer"); //CHANGE TO CORRECT SERVER!!
+
+echo json_encode($response);
+
+switch ($request["type"])
+{
+	case "login":
+		$response = "login, yeah we can do that";
+        	$rabbitRequest = array();
+        	$rabbitRequest['type'] = "Login";
+        	$rabbitRequest['username'] = $request['username'];
+        	$rabbitRequest['password'] = $request['password'];
+        	$rabbitRequest['message'] = "test WILL BE A KEY";
+        	$response = $client->send_request($rabbitRequest);
+        
+	break;
+	case "register":
+		$rabbitRequest = array();
+		$rabbitRequest['type'] = "registration";
+		$rabbitRequest['username'] = $request['username'];
+		$rabbitRequest['password'] = $request['password'];
+		$rabbitRequest['message'] = "test WILL BE A KEY";
+		$response = $client->send_request($rabbitRequest);
+		break;
+}
+echo json_encode($response);
+exit(0);
+
+
+
+?>
