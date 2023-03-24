68bc387f49b25f8cd6ebeda2fa91bcb342f80af5
efm24 <90346275+efm24@users.noreply.github.com>
2023-03-24 10:38:26 -0400

    Update event_logger.php

--- webServerStuff/event_logger.php
+++ webServerStuff/event_logger.php
@@ -2,20 +2,24 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-    
 
-    function event_logger($event,$message){
-                $fp = "events.log";
-        $event = "[" . date('Y-m-d H:i:s') . "] Event'" . $event . "': " . $message . "\n";
-        $fh = fopen($fp, "a");
-        fwrite($fh, $event);
-        fclose($fh);
-        $client = new rabbitMQClient();
-        $req = array(
-            'type' => 'log',
-            'log_type' => $event,
-            'log_message' => $log_message);
-        $response = $client->send_request($req);
-    }
+use rabbitMQClient;
+
+function event_logger($event, $message) {
+    $fp = "events.log";
+    $event = "[" . date('Y-m-d H:i:s') . "] Event'" . $event . "': " . $message . "\n";
+    $fh = fopen($fp, "a");
+    fwrite($fh, $event);
+    fclose($fh);
+    $client = new rabbitMQClient();
+    $req = array(
+        'type' => 'log',
+        'log_type' => $event,
+        'log_message' => $message
+    );
+    $response = $client->send_request($req);
+}
+
+
 
 ?>
